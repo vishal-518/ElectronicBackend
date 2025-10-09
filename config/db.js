3c877bf97186172f423a31be2dd57c88677ac263
MAC <mac@MACs-MBP.bbrouter>
2025-10-09 12:00:56 +0530

    change

--- config/db.js
+++ config/db.js
@@ -4,11 +4,11 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URL); // options not needed
+    await mongoose.connect(process.env.MONGO_DB_URL); 
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1); // stop server if DB connection fails
+    process.exit(1); 
   }
 };
 
