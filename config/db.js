c06e59e0d74d2ec3c5f61ac0c5f3bdd4736471a7
MAC <mac@MACs-MBP.bbrouter>
2025-10-09 11:24:08 +0530

    Updated signup and token logic

--- config/db.js
+++ config/db.js
@@ -1,19 +1,15 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            tls: true // ensure secure connection
-        });
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-        process.exit(1); // stop server if DB connection fails
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URL); // options not needed
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // stop server if DB connection fails
+  }
 };
 
 export default connectDB;
